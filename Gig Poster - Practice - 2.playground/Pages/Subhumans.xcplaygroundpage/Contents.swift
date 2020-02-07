a3993e72575ae1816ac63a40bb501dbc07d66020
Amanda <jkim@lcs.on.ca>
2020-02-07 11:08:30 -0500

    Background done

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -34,7 +34,9 @@ let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
 
 // Begin your solution here...
 
-
+// Make background colour
+canvas.fillColor = yellow
+canvas.drawRectangle(at: Point(x:0, y:0), width: 400, height: 600)
 /*:
  ## Use Source Control
  
